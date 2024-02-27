c8c340992ca13d1b8fb5de0c012bb765cf17bc07
yuuri0908 <148744622+yuuri0908@users.noreply.github.com>
2024-02-27 17:11:41 +0900

    再提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const Time =new Date();
+console.log(Time);
+
+
+const Year = Time.getFullYear();
+const Month = Time.getMonth();
+const Day = Time.getDate();
+
+console.log(Year+'年'+(Month + 1 )+'月'+Day+'日');
+
